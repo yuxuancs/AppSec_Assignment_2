25648eb2e45b25b85c84f95e026c234a4b795be8
yuxuancs <34151202+yuxuancs@users.noreply.github.com>
2022-10-23 16:16:29 -0400

    Update tests.py

--- LegacySite/tests.py
+++ LegacySite/tests.py
@@ -17,7 +17,7 @@ class MyTest(TestCase):
         super(MyTest,self).__init__()
         
     def test_1(self):
-        resp = Client.get('../gift/',{'director': "<script>alert('XSS attack!')</script>"})
+        resp = Client.get('/gift/',{'director': "<script>alert('XSS attack!')</script>"})
         string = bytes.decode(resp.content)
         if string.__contains__("&lt;script&gt"): # check whether "<" and ">" <=> "&lt" and "&gt"
             return "XSS test passed!"
@@ -25,7 +25,7 @@ class MyTest(TestCase):
             return None
 
     def test_2(self):
-        resp = Client(enforce_csrf_checks=True).post('../gift/1',{'amount':'1','username':'yuxuan_2'})
+        resp = Client(enforce_csrf_checks=True).post('/gift/1',{'amount':'1','username':'yuxuan_2'})
         if resp.status_code == 403:
             return "CRSF test passed!"
         else:
@@ -56,8 +56,7 @@ class MyTest(TestCase):
 def run_test():
     test = MyTest()
 #     assert test.test_1() != None, "XSS Error"
-#     assert test.test_2() != None, "CRSF Error"
-    print(os.getcwd())
+    assert test.test_2() != None, "CRSF Error"
     assert test.test_3() != None, "SQL Injection Error"
     assert test.test_4() != None, "Command Injection Error"
 
