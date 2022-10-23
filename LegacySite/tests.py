35f4abf97ba10a853d1b852ed458e8eed31976bb
yuxuancs <34151202+yuxuancs@users.noreply.github.com>
2022-10-23 16:00:57 -0400

    Update tests.py

--- LegacySite/tests.py
+++ LegacySite/tests.py
@@ -17,7 +17,7 @@ class MyTest(TestCase):
         super(MyTest,self).__init__()
         
     def test_1(self):
-        resp = Client.get('/gift/',{'director': "<script>alert('XSS attack!')</script>"})
+        resp = Client.get('../gift/',{'director': "<script>alert('XSS attack!')</script>"})
         string = bytes.decode(resp.content)
         if string.__contains__("&lt;script&gt"): # check whether "<" and ">" <=> "&lt" and "&gt"
             return "XSS test passed!"
@@ -25,15 +25,15 @@ class MyTest(TestCase):
             return None
 
     def test_2(self):
-        resp = Client(enforce_csrf_checks=True).post('/gift/1',{'amount':'1','username':'yuxuan_2'})
+        resp = Client(enforce_csrf_checks=True).post('../gift/1',{'amount':'1','username':'yuxuan_2'})
         if resp.status_code == 403:
             return "CRSF test passed!"
         else:
             return None
 
     def test_3(self):
-        cf_data = open('part1/SQL_Injection_steal_password.gftcrd')
-        cf_path = f'./tmp/SQLi_test_parser.gftcrd'
+        cf_data = open('../part1/SQL_Injection_steal_password.gftcrd')
+        cf_path = f'../tmp/SQLi_test_parser.gftcrd'
         card_data = extras.parse_card_data(cf_data.read(), cf_path)
         cf_data.close()
         signature = json.loads(card_data)['records'][0]['signature']
@@ -44,7 +44,7 @@ class MyTest(TestCase):
             return None
 
     def test_4(self):
-        card_path_name = open('part1/Injection.txt')
+        card_path_name = open('../part1/Injection.txt')
         # KG: Are you sure you want the user to control that input?
         ret_val = os.system(quote(f"./{CARD_PARSER} 2 {card_path_name} > tmp_file"))
         if ret_val != 0:
