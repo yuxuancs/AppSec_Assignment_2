18fd8f9d261d861561eba5e1e4b3c0bb7adf5828
yuxuancs <yuxuanhuangcs@gmail.com>
2022-10-24 01:06:19 -0400

    update

--- LegacySite/tests.py
+++ LegacySite/tests.py
@@ -56,7 +56,7 @@ class MyTest(TestCase):
 @pytest.mark.django_db
 def run_test():
     test = MyTest()
-#     assert test.test_1() != None, "XSS Error"
+    assert test.test_1() != None, "XSS Error"
     assert test.test_2() != None, "CRSF Error"
     assert test.test_3() != None, "SQL Injection Error"
     assert test.test_4() != None, "Command Injection Error"
